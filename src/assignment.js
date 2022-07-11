d441b39d4ab6999364dc450f9caef4663672f0f9
Venia1 <105131423+MaryveniaOkoye@users.noreply.github.com>
2022-07-11 20:51:58 +0000

    Made the functions to do the calculations

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let venia = 0;
+    for (let count = 0; count < arrayOfNumbers.length; count++) {
+      venia = venia + arrayOfNumbers[count]
+    } 
+  return venia;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,18 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let newNumbers = 0
+  
+    for (let count = 0; count < arrayOfNumbers.length; count++) {
+
+      
+      if (arrayOfNumbers[count] % 2 == 0) {
+        newNumbers++
+      }
+    }
+    return newNumbers
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
